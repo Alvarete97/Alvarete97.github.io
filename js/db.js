582b0be84b73ea95b082861f7ef890b9b3b1df52
Álvaro <74109026+Alvarete97@users.noreply.github.com>
2020-12-21 14:06:01 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,6 +1,6 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Ninetales:Asf271020@dbtfg.d4wbo.mongodb.net/dbTFG?retryWrites=true&w=majority";
+const uri = "mongodb+srv://Ninetales:<Contraseña>@dbtfg.d4wbo.mongodb.net/<BDD>?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
