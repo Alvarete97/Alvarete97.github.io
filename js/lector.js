ea901f835b9156936e4a0851bc1c29d0b3e061cd
Alvarete97 <alvaretesf.97@gmail.com>
2021-03-11 16:08:34 +0100

    2.3
    Prueba de lectura de json

--- js/lector.js
+++ js/lector.js
@@ -1,33 +1,72 @@
+/*
 
-const { Schema, model } = require ('db_tfg');
+const BdD = "../json/personajes.json";
+var datos = JSON.parse(BdD);
 
-const Pj_Schema = new Schema({
 
-	nombre: {type: String, required: true},
-	nacion: {type: String, required: true},
-	edad: {type: Number, required: false},
-	personalidad: {type: String, required: false},
-	genero: {type: Number, required: true},
-	descripcion: {type: String, required: true},
-	diseno: {type: String, required: true},
-	urlpj: {type:String, required: true},
-	urli: {type:String, required: true},
-	urlicopj: {type:String, required: true}
-})
+function load(){
 
-module.exports = model('Personaje', Pj_Schema);
+	console.log(datos);
+	//document.getElementById("nombre_pj").innerHTML = datos;
+}
+*/
 
-const Esc_Schema = new Schema({
 
-	nombre: {type: String, required: true},
-	nacion: {type: String, required: true},
-	localizacion: {type: String, required: false},
-	clima: {type: String, required: false},
-	descripcion: {type: String, required: true},
-	diseno: {type: String, required: true},
-	urlesc: {type:String, required: true},
-	urli: {type:String, required: true},
-	urlicoesc: {type:String, required: true}
-})
+    let requestURL = '../json/personajes.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'text';
+    request.send();
 
-module.exports = model('Escenario', Esc_Schema);
+    request.onload = function() {
+      const superHeroesText = request.response;
+      const superHeroes = JSON.parse(superHeroesText);
+      populateHeader(superHeroes);
+      showHeroes(superHeroes);
+    }
+
+    function populateHeader(jsonObj) {
+    	document.getElementById("nombre_pj").innerHTML = jsonObj[url].name;
+
+
+      const myH1 = document.createElement('h1');
+      myH1.textContent = jsonObj['squadName'];
+      header.appendChild(myH1);
+
+      const myPara = document.createElement('p');
+      myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+      header.appendChild(myPara);
+    }
+
+    function showHeroes(jsonObj) {
+      const heroes = jsonObj['members'];
+
+      for(let i = 0; i < heroes.length; i++) {
+        const myArticle = document.createElement('article');
+        const myH2 = document.createElement('h2');
+        const myPara1 = document.createElement('p');
+        const myPara2 = document.createElement('p');
+        const myPara3 = document.createElement('p');
+        const myList = document.createElement('ul');
+
+        myH2.textContent = heroes[i].name;
+        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+        myPara2.textContent = 'Age: ' + heroes[i].age;
+        myPara3.textContent = 'Superpowers:';
+
+        const superPowers = heroes[i].powers;
+        for(let j = 0; j < superPowers.length; j++) {
+          const listItem = document.createElement('li');
+          listItem.textContent = superPowers[j];
+          myList.appendChild(listItem);
+        }
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+
+        section.appendChild(myArticle);
+      }
+    }
--- js/url_datos.js
+++ js/url_datos.js
@@ -1,8 +1,46 @@
 
 
+
 var url = localStorage.getItem('url');
 //console.log(url);
 
+
+let requestURL = '../json/personajes.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'text';
+request.send();
+
+request.onload = function() {
+	const pjText = request.response;
+	const pj = JSON.parse(pjText);
+	cargar_datos(pj);
+	
+}
+
+
+
+
+function cargar_datos(jsonObj){
+	if (typeof(Storage) !== "undefined") {
+		document.getElementById("nombre_pj").innerHTML = jsonObj[url].name;
+
+
+
+		document.getElementById("nombre_pj").innerHTML  =  url;
+		console.log(url + " -> dentro del iframe");
+
+
+		
+	}else{
+		console.log("valor no definido dentro del iframe");
+	}
+
+}
+
+
+
+/*
 function cargar_datos(){
 	if (typeof(Storage) !== "undefined") {
 		document.getElementById("nombre_pj").innerHTML  =  url;
@@ -12,6 +50,6 @@ function cargar_datos(){
 	}
 
 }
-
+*/
 
 
